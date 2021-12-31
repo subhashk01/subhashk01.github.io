db85fc0e1dc621178901d24b1e998defd0cbce1a
subha <subha1@C02G73FPMD6R.tld>
2021-12-31 18:22:32 +0900

    changed coursework implementation and color

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 70,
+        "value": 80,
         "density": {
           "enable": true,
           "value_area": 800
